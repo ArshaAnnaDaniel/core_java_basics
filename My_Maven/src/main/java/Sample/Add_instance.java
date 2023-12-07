559945f10ead0f2688f053d37ebf1e9f2494bf8e
Arsha <Arsha@ARSHA-LTP>
2023-12-07 11:40:36 +0530

    updated the code

--- My_Maven/src/main/java/Sample/Add_instance.java
+++ My_Maven/src/main/java/Sample/Add_instance.java
@@ -2,19 +2,13 @@ package Sample;
 
 public class Add_instance {
 	public void  add(int a,int b) {
-		
 		int sum=a+b;
 		System.out.println("sum is:"+sum);
-		
-	}
-	
-	
-	
-
+		}
 	public static void main(String[] args) {
 	Add_instance ob=new Add_instance();
 	ob.add(5, 10);
-	
+	System.out.println("arsha");
 
 	}
 
